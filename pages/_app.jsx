325eafcba1944b54e3aa751108eb3623f6ada83c
unclejee <swatxhim@outlook.com>
2024-05-15 22:57:53 +0800

    统计代码

---
+++ pages/_app.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Head from 'next/head';
+
+const GoogleAnalyticsScript = () => {
+    return (
+        <>
+            <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=G-7BDPKHVLRK`}
+            />
+            <script
+                type="text/javascript"
+                dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-7BDPKHVLRK');`,
+                }}
+            />
+            <script
+                type="text/javascript"
+                dangerouslySetInnerHTML={{
+                    __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "lpo3k0x6fh");`,
+                }}
+            />
+        </>
+    );
+};
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <GoogleAnalyticsScript />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+export default MyApp;
