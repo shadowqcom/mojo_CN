18b4e8dc0e3771362309261e28750cbf555d9b71
shadowqcom <42204012+shadowqcom@users.noreply.github.com>
2024-09-17 04:11:58 +0800

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -20,6 +20,11 @@ const GoogleAnalyticsScript = () => {
                     __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "lpo3k0x6fh");`,
                 }}
             />
+            <script 
+                async 
+                src={'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9179261695088632'}
+                crossorigin="anonymous"
+            />
         </>
     );
 };
